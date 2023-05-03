02224c93d2c36e0cd1858a23f50b2604094b1d91
YusufHasan94 <yhpolok@gmail.com>
2023-05-03 11:51:04 +0600

    responsive chefs recipes section

--- src/Components/Main/Recipes/Recipes.jsx
+++ src/Components/Main/Recipes/Recipes.jsx
@@ -10,15 +10,15 @@ const Recipes = () => {
         <div className='my-4'>
             <h1 className='text-center'>Chef's Recipes</h1>
             <Container className='my-5'>
-                <div className='d-flex gap-5 bg-light p-4'>
-                    <div className='w-50 my-auto'>
+                <div className='d-flex flex-column-reverse flex-md-row gap-5 bg-light p-4'>
+                    <div className='w-100 my-auto'>
                         <h1>{name}</h1>
                         <p className='my-1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis beatae deserunt molestiae dolore cum quia reprehenderit est eum enim dolorum? Modi cumque, dolorum non, minus odio quidem doloribus eos, doloremque deserunt nihil veritatis laboriosam vero necessitatibus esse voluptatum tempore totam?</p>
                         <p className='my-1 fs-5'><span className='fw-semibold'>Likes: </span>{likes}</p>
                         <p className='my-1 fs-5'><span className='fw-semibold'>number of recipes: </span>{number_of_recipes}</p>
                         <p className='my-1 fs-5'><span className='fw-semibold'>years of experience: </span>{years_of_experience}</p>
                     </div>
-                    <div className='w-50 d-flex justify-content-center'>
+                    <div className='w-100 d-flex justify-content-center'>
                         <img src={picture} className='w-100' alt="" />
                     </div>
                 </div>
